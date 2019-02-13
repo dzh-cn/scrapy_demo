84813fb6cf53364a62717bd7c3390456e7006904
董智华 <dongzhihua@jd.com>
2019-02-13 10:07:36 +0800

    'sql'

--- douban_movie/pipelines.py
+++ douban_movie/pipelines.py
@@ -11,7 +11,7 @@ import pymysql
 
 class DoubanMoviePipeline(object):
     def __init__(self):
-        self.connect = pymysql.connect("localhost", "test", "test", "test")
+        self.connect = pymysql.connect("172.25.4.218", "m_test", "jZetUn", "test")
         self.cursor = self.connect.cursor()
         self.sql = """insert into movie_top_250(url,number,name,time,director,scenario,starts,type,district,language,time_desc) value (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) on duplicate key update name=(name)"""
 
